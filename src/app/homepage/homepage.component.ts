957bfbdc7d8a21a1a7a906fc73562c78d76f8f43
suchjose84 <suchjose@live.com>
2023-07-15 15:44:54 +0800

    fix fix

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
+  styles: [
+    `@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap');`
+  ]
 })
 export class HomepageComponent {
 
