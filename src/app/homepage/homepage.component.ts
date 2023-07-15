67c8d47e0551e7705847df2b71e4d4f214154983
suchjose84 <suchjose@live.com>
2023-07-15 16:17:03 +0800

    check check

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,11 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css'],
-  styles: [
-    `@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap');`
-  ]
+  styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
+  showDiv: boolean = false;
 
 }
