6489cc74577388807c1ce6f40103105a697a0a43
suchjose84 <suchjose@live.com>
2023-07-15 09:51:34 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { TemplateComponent } from './template/template.component';
+import { HeaderComponent } from './template/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    TemplateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
